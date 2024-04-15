6a8f6f1ad1e1e20b7f47469141ede770a81299f4
Siva R <166374356+Siva1309@users.noreply.github.com>
2024-04-15 14:19:58 +0530

    Update Exp2_Demultiplexer1to8.v

--- Exp2_Demultiplexer1to8.v
+++ Exp2_Demultiplexer1to8.v
@@ -1,9 +1,25 @@
-module demux_8(s,d,y);
-input [2:0]s;
+module demux(d, sel, z);
 input d;
-output [7:0]y;
-
-
-
-
+input [2:0] sel;
+output [7:0] z;
+reg [7:0] z;
+always @(d or sel)
+begin
+if( sel==3'b000)
+z[0]=d;
+else if( sel==3'b001)
+z[1]=d;
+else if( sel==3'b010)
+221-d;
+else if( sel==3'b011)
+z[3]=d;
+else if( sel==3'b100)
+z[4]=d;
+else if( sel=3'b101)
+z[5]=d;
+else if( sel==3'bl10)
+z[6]=d;
+else
+z[7]=d;
+end
 endmodule
